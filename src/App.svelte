1b076c524c25055231dd10ce8b84200e935b7cb7
Anthony MARQUES <anthony.marqu3s@gmail.com>
2020-07-31 16:26:50 +0200

    Add svelte bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,34 @@
 <script>
-	import gomoku from 'ic:canisters/gomoku';
-	export let name;
+  import { Button, Col, Row } from "sveltestrap";
 
-	gomoku.greet(window.prompt("Enter your name:")).then(greeting => {
-		window.alert(greeting);
-	});
+  import gomoku from "ic:canisters/gomoku";
+  export let name;
+  export let greeting;
+
+  gomoku.greet("lol").then((resp) => {
+    greeting = resp;
+  });
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<head>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+</head>
+<Row>
+  <Col>
+    <h1>Hello {name}!</h1>
+  </Col>
+</Row>
+<!-- <Row><h1>{greeting}</h1></Row> -->
+<Row>
+  <Col>
+    <Button color="primary" outline>Hello World!</Button>
+  </Col>
+</Row>
